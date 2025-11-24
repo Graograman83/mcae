81d3b5042c62d10172e5da46ec4cadc539aef230
Graograman <t_stelljes@yahoo.de>
2025-11-24 20:18:56 +0100

    add historical rates

--- force-app/main/default/lwc/exchangeRates/exchangeRates.js
+++ force-app/main/default/lwc/exchangeRates/exchangeRates.js
@@ -1,9 +1,16 @@
 import { LightningElement, wire } from 'lwc';
-import getExchangeRates from '@salesforce/apex/ExchangeRateController.getExchangeRates';
+import getHistoricalExchangeRates from '@salesforce/apex/ExchangeRateController.getHistoricalExchangeRates';
+import getLatestExchangeRates from '@salesforce/apex/ExchangeRateController.getLatestExchangeRates';
 import getSymbols from '@salesforce/apex/ExchangeRateController.getSymbols';
 
 export default class ExchangeRates extends LightningElement {
     base = 'EUR';
+    get baseHistoricalParam() {
+        return this.mode === 'historical' ? this.base : undefined;
+    }
+    get baseLatestParam() {
+        return this.mode === 'latest' ? this.base : undefined;
+    }
     columns = [
         { label: 'Currency', fieldName: 'currency', type: 'text' },
         { label: 'Rate', fieldName: 'rate', type: 'number' }
@@ -13,6 +20,19 @@ export default class ExchangeRates extends LightningElement {
         return Object.entries(this.currencies).map(([key, value]) => ({ label: key + ' - ' + value, value: key }));
     };
     error;
+    historicalDate;
+    get isHistoricalMode() {
+        return this.mode === 'historical';
+    }
+    get isTimeseriesMode() {
+        return this.mode === 'timeseries';
+    }
+    get maxDate() {
+        let date = new Date();
+        date.setDate(date.getDate() - 1);
+        return date = date.toISOString().split('T')[0];
+    }
+    minDate = '2000-01-01';
     modeOptions = [
         { label: 'Latest', value: 'latest' },
         { label: 'Historical', value: 'historical' }
@@ -21,17 +41,34 @@ export default class ExchangeRates extends LightningElement {
     quotes = [];
     ratesData = {};
     get rates() {
+        if (this.isHistoricalMode && !this.historicalDate) {
+            return [];
+        }
         return Object.entries(this.ratesData).filter(([key]) => this.quotes.includes(key)).map(([key, value]) => ({ id: key, currency: key + ' - ' + this.currencies[key], rate: value }));
     };
     timestamp
 
-    @wire(getExchangeRates, { base: '$base' }) 
-    wiredExchangeRates({ error, data }) {
+    @wire(getLatestExchangeRates, { base: '$baseLatestParam' }) 
+    wiredLatestExchangeRates({ error, data }) {
         if (data) {
             this.timestamp = new Date(data.timestamp);
             this.ratesData = data.rates;
+            console.log('getLatestExchangeRates', this.ratesData, this.timestamp)
         } else if (error) {
             this.error = error;
+            console.log('getLatestExchangeRates error', this.error)
+        }
+    }
+
+    @wire(getHistoricalExchangeRates, { base: '$baseHistoricalParam', historicalDate: '$historicalDate' }) 
+    wiredHistoricalExchangeRates({ error, data }) {
+        if (data) {
+            this.timestamp = new Date(data.timestamp);
+            this.ratesData = data.rates;
+            console.log('getHistoricalExchangeRates', this.ratesData, this.timestamp)
+        } else if (error) {
+            this.error = error;
+            console.log('getHistoricalExchangeRates error', this.error)
         }
     }
 
@@ -50,6 +87,11 @@ export default class ExchangeRates extends LightningElement {
 
     handleModeChange(event) {
         this.mode = event.detail.value;
+    }
+
+
+    handleHistoricalDateChange(event) {
+        this.historicalDate = event.detail.value;
     }
 
     handleQuoteChange(event) {
